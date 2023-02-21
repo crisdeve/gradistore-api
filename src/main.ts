1f56cb98deaf1d80d5b5fc5d1485ddc535a28893
crisdeve <cristian@gradiweb.com>
2023-02-21 14:09:26 -0500

    prepare to deploy

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const port = process.env.PORT ? Number(process.env.PORT) : 3003;
+  const port = process.env.PORT ? Number(process.env.PORT) : 8080;
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(
@@ -19,6 +19,6 @@ async function bootstrap() {
   // ? App configuration
   app.enableCors();
   await app.listen(port);
-  console.log(`Server is ready to listen on port :${port}`);
+  console.log(`Server on port :${port}`);
 }
 bootstrap();
